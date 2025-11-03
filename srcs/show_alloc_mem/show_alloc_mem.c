dee71a1833d582580348cc80a393645d9e44eccc
Alexandre Ferreira Da Silva <alexafer@shi-r12-p3.s19.be>
2025-11-03 16:51:33 +0100

    ajotus

--- srcs/show_alloc_mem/show_alloc_mem.c
+++ srcs/show_alloc_mem/show_alloc_mem.c
@@ -6,7 +6,7 @@
 /*   By: alexafer <alexafer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 16:12:08 by alexafer          #+#    #+#             */
-/*   Updated: 2025/11/03 16:25:43 by alexafer         ###   ########.fr       */
+/*   Updated: 2025/11/03 16:51:20 by alexafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,37 @@
 
 void	show_alloc_mem(void)
 {
-	ft_putstr_fd("SHOW MEMORY", 1);
+	t_zone	*zone;
+	t_block	*block;
+	int		i;
+	int		j;
+
+	ft_putstr_fd(REDB "SHOW MEMORY:" CRESET "\n", 1);
+	i = 0;
+	zone = g_global;
+	while (zone)
+	{
+		j = 0;
+		ft_putstr_fd("  ", 1);
+		ft_putnbr_fd(i, 1);
+		ft_putstr_fd(". Size Zone : ",1);
+		ft_putnbr_fd(zone->size, 1);
+		ft_putstr_fd("\n",1);
+		block = (t_block *)(zone + 1);
+		while (block)
+		{
+			ft_putstr_fd("    ", 1);
+			ft_putnbr_fd(i, 1);
+			ft_putstr_fd(".", 1);
+			ft_putnbr_fd(j, 1);
+			ft_putstr_fd(". Size Block : ", 1);
+			ft_putnbr_fd(block->size, 1);
+			ft_putstr_fd("\n", 1);
+			j++;
+			block = block->next;
+		}
+		zone = zone->next;
+		i++;
+	}
 }
+
