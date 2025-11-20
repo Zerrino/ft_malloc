1a4b942fb61ccd6876504fa69f7ee69f708ae9f9
Alexandre Ferreira Da Silva <alexafer@shi-r12-p3.s19.be>
2025-11-20 14:21:51 +0100

    aaa

--- srcs/free/free.c
+++ srcs/free/free.c
@@ -6,7 +6,7 @@
 /*   By: alexafer <alexafer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 13:16:35 by alexafer          #+#    #+#             */
-/*   Updated: 2025/11/20 13:42:32 by alexafer         ###   ########.fr       */
+/*   Updated: 2025/11/20 14:20:56 by alexafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void 	ft_free(void *ptr)
 	t_block	*l_block;
 	size_t	total;
 
-	write(1, "test\n", 5);
 	if (((size_t)ptr % 0x10) != 0 || !ptr)
 		return ;
 	zone = g_global;
--- srcs/wrappers.c
+++ srcs/wrappers.c
@@ -6,7 +6,7 @@
 /*   By: alexafer <alexafer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 14:00:17 by alexafer          #+#    #+#             */
-/*   Updated: 2025/11/20 14:19:35 by alexafer         ###   ########.fr       */
+/*   Updated: 2025/11/20 14:21:26 by alexafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ void *malloc(size_t size)
 {
 	return (ft_malloc(size));
 }
-void  free(void *p)				{
-	return ;
-	write(1, "test\n", 5);
-	ft_free(p); }
-void *realloc(void *p,size_t size)	{
-
-	write(1, "test\n", 5);
-	return ft_realloc(p,size); }
+void  free(void *p)
+{
+	ft_free(p);
+}
+void *realloc(void *p,size_t size)
+{
+	return ft_realloc(p,size);
+}
